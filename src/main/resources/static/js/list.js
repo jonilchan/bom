a862b6c7f0f49cf656a23dc6ce72d7eb77492257
jonil <jonilchan@foxmail.com>
2022-12-05 19:10:40 +0800

    更新批量物料清单页面

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -6,35 +6,36 @@ layui.use(['table', 'layer', "form"], function () {
     var tableIns = table.render({
         elem: '#list',
         url: 'mdItem/getMdItemListByIds',
-        cellMinWidth: 95,
-        page: true,
+        cellMinWidth: 50,
+        page: false,
         height: "full-125",
         limits: [10, 15, 20, 25],
         limit: 10,
         toolbar: "#toolbarDemo",
         id: "list",
         cols: [[
-            {field: "item_id", title: '物料ID', fixed: "true", width: 150},
-            {field: 'item_code', title: '物料编码', minWidth: 50, align: "center"},
-            {field: 'item_name', title: '物料名称', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'item_class_code', title: '物料分类代码', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'item_class_name', title: '物料分类名称', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'item_quantity', title: '物料数量', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'crt_user', title: '创建者', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'crt_time', title: '创建时间', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'upd_user', title: '更新者', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"},
-            {field: 'upd_time', title: '更新时间', minWidth: 300, templet: '#userListBar', fixed: "right", align: "center"}
-        ]]
+            {field: "itemId", title: '物料ID', fixed: "right", width: 150, align: "center"},
+            {field: 'itemCode', title: '物料编码', minWidth: 50, align: "center"},
+            {field: 'itemName', title: '物料名称', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'itemClassCode', title: '物料分类代码', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'itemClassName', title: '物料分类名称', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'itemQuantity', title: '物料数量', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'crtUser', title: '创建者', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'crtTime', title: '创建时间', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'updUser', title: '更新者', minWidth: 50, fixed: "right", align: "center"},
+            {field: 'updTime', title: '更新时间', minWidth: 50, fixed: "right", align: "center"}
+        ]],
+        response:{
+            statusName:'code', //规定返回的状态码字段为code
+            statusCode:200 //规定成功的状态码味200
+        },
     });
 
     // 多条件搜索
     $(".search_btn").on("click", function () {
         table.reload("list", {
-            page: {
-                curr: 1
-            },
             where: {
-                ids: $("input[name='ids']").val().split(",")//  ids
+                ids: $("input[name='ids']").val()//  ids
             }
         })
     });
